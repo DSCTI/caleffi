c12aa7a696c6f7f54cdcd5b03b08b55eae263f30
danilosc <danilosc@msn.com>
2020-01-30 05:39:55 -0200

    sa
    sa

--- geo.js
+++ geo.js
@@ -5,7 +5,21 @@ function onDeviceReady() {
 
 window.plugins.insomnia.keepAwake();
 
+
+var lastUpdateTime,
+minFrequency = 10*1000,
+
+var options = {
+ timeout : 60*60*1000,
+ maxAge: 0,
+ enableHighAccuracy: true
+};
+
 function onSuccess(position) {
+
+ var now = new Date();
+    if(lastUpdateTime && now.getTime() - lastUpdateTime.getTime() < minFrequency){
+      
 
 var location1 = aluno + "-location";
 var location = localStorage.getItem(location1);
@@ -15,17 +29,16 @@ localStorage.setItem(location1, result);
 
 alert (result);
 
+
+        return;
+    }
+    lastUpdateTime = now;
+
     } 
 
 function onError(error) {
     
 }
-
-var options = {
- timeout : 60*60*1000,
- maxAge: 0,
- enableHighAccuracy: true
-};
 
 
 navigator.geolocation.watchPosition(onSuccess,onError,options);
