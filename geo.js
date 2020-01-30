6dc9a2993613fb98d36e70710e26233677db9ecd
danilosc <danilosc@msn.com>
2020-01-30 04:53:54 -0200

    SAS
    SA

--- geo.js
+++ geo.js
@@ -14,15 +14,10 @@ var aula = localStorage.getItem("aula"); if (aula == 0) {} else {
 var location1 = aluno + "-location";
 var location = localStorage.getItem(location1);
 
-var result =  position.coords.latitude    + '//' +  position.coords.longitude     + 'end'  + location;
+var result =  position.coords.latitude + '//' +  position.coords.longitude  + 'end'  + location;
 localStorage.setItem(location1, result);
 alert (result);
     } 
-
-  function onError(error) {
-
-
-    }
 
     var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 10000 });
 
