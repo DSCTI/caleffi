79fa519bae11f8edc134b46215a1561e182a7a68
danilosc <danilosc@msn.com>
2020-01-30 06:10:00 -0200

    sasa

--- geo.js
+++ geo.js
@@ -10,16 +10,18 @@ window.plugins.insomnia.keepAwake();
 
     function onSuccess(position) {
       
+setTimeout(function(){ 
+
 var location = localStorage.getItem(location1);
 var result =  position.coords.latitude + '//' + position.coords.longitude  + 'end'  + location;
 localStorage.setItem(location1, result);
 
-alert (result);
+alert (result); }, 10000);
 
 }
 
 function onError(error) {  }
 
-var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 3000, enableHighAccuracy: false });
+var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 3000, enableHighAccuracy: true });
 
 }
