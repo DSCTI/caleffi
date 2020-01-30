7070d399be8f89e51e9468d499f7c276670df787
danilosc <danilosc@msn.com>
2020-01-30 14:04:14 -0200

    sassa
    a

--- geo.js
+++ geo.js
@@ -7,22 +7,16 @@ var location1 = aluno + "-location";
 function onDeviceReady() {
 
 window.plugins.insomnia.keepAwake();
-
- document.addEventListener("backbutton", function (e) {
-        e.preventDefault();
-    }, false );
+document.addEventListener("backbutton", function (e) {   e.preventDefault(); }, false );
 
 
-
-    function onSuccess(position) {
-      
-
+function onSuccess(position) {
 var location = localStorage.getItem(location1);
 
 var result1 = position.coords.latitude; 
 var result2 = position.coords.longitude;
 
-var result = '///' + result1  + '//' +  result2  + location;
+var result = "c" + result1  + "e" +  result2  + location;
 
 localStorage.setItem(location1, result);
 
@@ -30,7 +24,7 @@ alert (result);
 
 }
 
-function onError(error) {  }
+function onError(error) { alert ("erro");  }
 
 
 var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 10000, enableHighAccuracy: false });
