9d5f19a4e58cda0f8dad5dbaef44d6d3d3f15dcb
danilosc <danilosc@msn.com>
2020-01-31 18:34:06 -0200

    sa
    sa

--- geolocation.js
+++ geolocation.js
@@ -2,7 +2,7 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 var aluno = localStorage.getItem("cpf");
-var location1 = aluno + "-location";
+var geo1 = aluno + "-location";
 
 function onDeviceReady() {
 
@@ -11,18 +11,18 @@ document.addEventListener("backbutton", function (e) {  e.preventDefault(); }, f
 
 function onSuccess(position) {
 
-var location2 = localStorage.getItem(location1);
-var result1 = position.coords.latitude;  if (result1 == null) { var result1 = "0"; }
-var result2 = position.coords.longitude; if (result2 == null) { var result2 = "0"; }
-var speed = position.coords.speed; if (speed == null) { var speed = "1"; }  if (speed == "0") { var speed = "1"; }
+var geo2 = localStorage.getItem(geo1);
+var result1 = position.coords.latitude;  if (result1 == null) { var result1 = "1"; }
+var result2 = position.coords.longitude; if (result2 == null) { var result2 = "1"; }
+var speed = position.coords.speed; if (speed == null) { var speed = "1"; }  
 
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 
 var aula = localStorage.getItem("aula"); 
 if (aula == null) {} else {
-var result = "s"+result1+"e"+result2+"e" + hours + "e" + speed + location2;
-localStorage.setItem(location1, result); }
+var result = "s"+result1+"e"+result2+"e" + hours + "e" + speed + geo2;
+localStorage.setItem(geo1, result); }
 
 }
 
