9796980d3a9dce47595618e6b44026989a80333b
danilosc <danilosc@msn.com>
2020-02-02 21:09:56 -0200

    sa
    sa

--- geolocation.js
+++ geolocation.js
@@ -13,11 +13,11 @@ function onSuccess(position) {
 var result1 = position.coords.latitude;   if (result1 == null) {var result1 = "1";} 
 var result2 = position.coords.longitude;  if (result2 == null) {var result2 = "1";}
 var speed = position.coords.speed;        if (speed == null) {var speed = "1";}
-speed = speed.substring(0, 2);              
+          
 
 alert (result1);
 alert (result2);
-alert (speed);
+alert(speed.slice(0,2));
 
 } 
 
