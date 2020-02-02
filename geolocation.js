96ad3644124342d51d1a5d891a016cfeb5ae04f3
danilosc <danilosc@msn.com>
2020-02-02 19:19:01 -0200

    sds
    ds

--- geolocation.js
+++ geolocation.js
@@ -21,18 +21,11 @@ var speed = (speed.substring(0, 2));
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 
-if (aula    == null) {} else {
-if (result1 == null) {} else {
-if (result2 == null) {} else {
-if (speed   == null) {} else {
-
 var result = "s"+result1+"e"+result2+"e" + hours + "e" + speed + geo2;
 
 if (result  == null) {} else {
-
 localStorage.setItem(geo1, result);
-
-}}}}}
+}
 
 
 }
