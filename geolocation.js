9fca1cd6ac0a0820f74ffc25fce9be6fe4bc88da
danilosc <danilosc@msn.com>
2020-02-02 20:24:58 -0200

    fdf
    d

--- geolocation.js
+++ geolocation.js
@@ -17,7 +17,7 @@ var geo2 = localStorage.getItem(geo1);
 var result1 = position.coords.latitude;   if (result1 == null) {var result1 = "1";}
 var result2 = position.coords.longitude;  if (result2 == null) {var result2 = "1";}
 var speed = position.coords.speed;        
-var speed = (speed.substring(0, 2));      if (speed == null) {var speed = "1";} 
+var speed = speed.substring(0, 2);        if (speed == null) {var speed = "1";} 
 
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
