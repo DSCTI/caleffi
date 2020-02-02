654ebde18cec04e1de6616384d2af212492a11e9
danilosc <danilosc@msn.com>
2020-02-02 19:36:47 -0200

    sdsd
    dsds

--- geolocation.js
+++ geolocation.js
@@ -12,11 +12,12 @@ document.addEventListener("backbutton", function (e) {  e.preventDefault(); }, f
 
 function onSuccess(position) {
 
-var geo2 = localStorage.getItem(geo1);
-var result1 = position.coords.latitude;  
-var result2 = position.coords.longitude; 
-var speed = position.coords.speed; 
-var speed = (speed.substring(0, 2));
+var geo2 = localStorage.getItem(geo1);    if (geo != null) {
+
+var result1 = position.coords.latitude;   if (result1 == null) {var result1 == "1";}
+var result2 = position.coords.longitude;  if (result2 == null) {var result2 == "1";}
+var speed = position.coords.speed;        
+var speed = (speed.substring(0, 2));      if (speed == null) {var speed == "1";} 
 
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
@@ -27,7 +28,7 @@ if (result  != null) {
 localStorage.setItem(geo1, result);
 }
 
-}
+} }
 
 function onError(error) {  }
 
