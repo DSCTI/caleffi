ec9347cfc20107f0253e21d225c595c660154a40
danilosc <danilosc@msn.com>
2020-02-02 18:13:55 -0200

    a
    sas

---
+++ geolocation-check.js
@@ -0,0 +1,13 @@
+//faz a checagem do GPS no primeiro acesso e exibe diálogo de permissão
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+
+function onSuccess(position) {  }
+
+function onError(error) {  }
+
+var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000, enableHighAccuracy: true  });
+
+}
--- geolocation.js
+++ geolocation.js
@@ -14,7 +14,11 @@ function onSuccess(position) {
 var geo2 = localStorage.getItem(geo1);
 var result1 = position.coords.latitude;  if (result1 == null) { var result1 = "1"; }
 var result2 = position.coords.longitude; if (result2 == null) { var result2 = "1"; }
-var speed = position.coords.speed; if (speed == null) { var speed = "1"; }  
+var speed = position.coords.speed;
+
+var speed = speed[0] + speed[1];
+
+if (speed == null) { var speed = "1"; }  
 
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
