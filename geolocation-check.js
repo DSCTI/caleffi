2d558c4baf2a325feafa77e6d6b9edcb6e046f6e
danilosc <danilosc@msn.com>
2020-02-03 05:29:28 -0200

    dsd
    ds

--- geolocation-check.js
+++ geolocation-check.js
@@ -1,3 +1,6 @@
+//desabilita botão voltar
+document.addEventListener("backbutton", function (e) {  e.preventDefault(); }, false );
+
 //faz a checagem do GPS no primeiro acesso e exibe diálogo de permissão
 
 document.addEventListener("deviceready", onDeviceReady, false);
