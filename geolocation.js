645831601f1eb85f9a547133e8363398ade9e589
danilosc <danilosc@msn.com>
2020-02-17 18:39:50 -0300

    asa
    sa

--- geolocation.js
+++ geolocation.js
@@ -18,6 +18,15 @@ var result2 = position.coords.longitude;
 var speed = position.coords.speed;
 var speed = Math.floor(speed);
 
+if (speed != null) {
+
+var speed1 = speed.charAt(0);
+var speed2 = speed.charAt(1);
+var speed3 = speed.charAt(2);
+var speed4 = speed.charAt(3);
+
+var speed = speed1 + speed2 + speed3 + speed4;
+
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 var result = "s"+result1+"e"+result2+"e" + hours + "e" + speed + geo;
@@ -34,7 +43,7 @@ if (aluno == null) {location.href = "aluno.html";}
 
 }}}
 
-}
+} }
 
 function onError(error) {  }
 
