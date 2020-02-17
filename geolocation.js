68806627323b7e0580275189b92d7348b2b38517
danilosc <danilosc@msn.com>
2020-02-17 17:36:52 -0300

    dsds
    dsdsds

--- geolocation.js
+++ geolocation.js
@@ -17,6 +17,21 @@ var result1 = position.coords.latitude;
 var result2 = position.coords.longitude;  
 var speed = position.coords.speed;              
 
+var speed1 = (speed.charAt(0)); 
+var speed2 = (speed.charAt(1)); 
+var speed3 = (speed.charAt(2)); 
+var speed4 = (speed.charAt(3)); 
+
+var speed = speed1 + speed2 + speed3 + speed4;  
+
+alert  (speed);
+
+var speed0 = (speed.charAt(0)); 
+alert  (speed0);
+
+if (speed0 > 0) {
+
+
 var now = new Date(Date.now());
 var hours = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
 var result = "s"+result1+"e"+result2+"e" + hours + "e" + speed + geo;
@@ -35,7 +50,7 @@ if (aluno == null) {location.href = "aluno.html";}
 
 }}}}
 
-} 
+} }
 
 function onError(error) {  }
 
